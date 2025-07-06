48c1f89ae397c3c56c08fc45ed7a7e1bb9487411
sauravdani <sauravdani7@gmail.com>
2025-07-06 11:36:03 +0530

    Initial commit
    Add initial project files and setup.

--- frontend-angular-dashboard/src/app/app.component.ts
+++ frontend-angular-dashboard/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Log } from '../../log.model';
+import { LogService } from './services/log.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'frontend-angular-dashboard';
+export class AppComponent implements OnInit{
+  logs: Log[] = [];
+
+  constructor(private logService: LogService) {}
+
+  ngOnInit() {
+    this.logService.getLogs().subscribe({
+      next: (data) => this.logs = data,
+      error: (err) => console.error('Error fetching logs', err)
+    });
+  }
 }
