5e0144921eaf9d063005ebba33132a4bf50257a1
sauravdani <97287044+sauravdani@users.noreply.github.com>
2026-01-31 02:21:43 +0530

    collected raw Log data to work on

--- frontend-angular-dashboard/src/app/models/log.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Log {
-    id: number;
-    timestamp: string;
-    service: string;
-    level: string;
-    message: string;
-}
--- log-collector-springboot/src/main/java/com/saurav/logcollector/repository/LogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.saurav.logcollector.repository;
-
-import com.saurav.logcollector.model.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogRepository extends JpaRepository<Log, Long> {
-}
